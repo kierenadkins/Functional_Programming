11313c2a2748f495bdbb04970ee9eacf555c57df
kierenadkins <75570969+kierenadkins@users.noreply.github.com>
2023-11-03 22:12:45 +0000

    Make the code follow clojure naming patterns + comments

--- src/functional_programming/core.clj
+++ src/functional_programming/core.clj
@@ -2,35 +2,62 @@
   (:gen-class))
 
 ;Task One
-(defn trinary-to-decimal  ([trinary-str]
+;To solve this problem i have used recurision, i first match the string using regex pattern matching.
+;if false we simply return 0
+;if true we call the function when we convert the trinary-str to a lazy sequence of ints and reverse the string
+;we then apply the calculation recursively
+
+(defn convert-trinary-to-decimal  ([trinary-str]
   (if (re-matches #"^[0-2]+" trinary-str) ;Matches against string containing multiple characters but all must be 0, 1 or 2.
-    (trinary-to-decimal
+    (convert-trinary-to-decimal
       (->> trinary-str
-           (map #(Character/getNumericValue %));code to convert string to numbers is found here https://stackoverflow.com/questions/4836768/create-a-list-from-a-string-in-clojure
+           (map #(Character/getNumericValue %));code to convert string to lazy sequence of numbers code is found here https://stackoverflow.com/questions/4836768/create-a-list-from-a-string-in-clojure
            (reverse))
       0 0) ;calls the recursive trinary method with a reversed vector of ints which match the string
     0))
-  ([trinary-str, index ,total]
-   (if (= index (count trinary-str))                         ;recursive condition, if we get past the last index return total
+  ([trinary-lazy, index ,total]
+   (if (= index (count trinary-lazy))                         ;recursive condition, if we get past the last index return total
      total
-     (recur trinary-str, (inc index), (int (+ total (* (nth trinary-str index) (Math/pow 3 index))))) ;provides calculation to the digit in index
+     (recur trinary-lazy, (inc index), (int (+ total (* (nth trinary-lazy index) (Math/pow 3 index))))) ;provides calculation to the digit in index
      )))
 
 ;;Task Two
-
-(defn translate_codon_to_protein[codon]
-  (let [codon-to-protein-map {:AUG "Methionine" :UUU "Phenylalanine" :UUC "Phenylalanine" :UUA "Leucine" :UUG "Leucine" :UCU "Serine" :UCC "Serine" :UCA "Serine" :UCG "Serine" :UAU "Tyrosine" :UAC "Tyrosine" :UGU "Cysteine" :UGC "Cysteine" :UGG "Tryptophan" :UAA "STOP" :UAG "STOP" :UGA "STOP" }]
+;Here we have defined a map with only the scope of the function which will act as our main codon to protein conversion
+;we use the get to return the value of the key and convert the codon string to a key using (keyword)
+(defn convert-codon-to-protein[codon]
+  (let [codon-to-protein-map {
+                              :AUG "Methionine"
+                              :UUU "Phenylalanine"
+                              :UUC "Phenylalanine"
+                              :UUA "Leucine"
+                              :UUG "Leucine"
+                              :UCU "Serine"
+                              :UCC "Serine"
+                              :UCA "Serine"
+                              :UCG "Serine"
+                              :UAU "Tyrosine"
+                              :UAC "Tyrosine"
+                              :UGU "Cysteine"
+                              :UGC "Cysteine"
+                              :UGG "Tryptophan"
+                              :UAA "STOP"
+                              :UAG "STOP"
+                              :UGA "STOP" }]
     (get codon-to-protein-map (keyword codon)))) ;converts the string into a keyword which is used to get the value from the map
 
-(defn translate_rna_to_amino_acids [rna]
-  ;Equivalent to (map #(translate_codon_to_protein %) (map #(apply str %) (partition 3 rna)))
-  (let  [result ( ->> rna           ;threads the last arugmenet to each function
+;This function effectively splits the rna-sequence into a lazy sequence of characters, before using the map function
+;to apply the function "apply str" to each col of the lazy sequence to turn the 3 characters into a codon
+;we then use another map function to apply the convert-codon-to-protein to each codon in the lazy sequence
+;finally we use the take-while to only return the lazy sequence which contains codons until the "Stop"
+(defn convert-rna-sequence-to-amino-acids [rna-sequence]
+  ;Equivalent to (map #(convert-codon-to-protein %) (map #(apply str %) (partition 3 rna)))
+  (let  [amino-acids ( ->> rna-sequence           ;threads the last arugmenet to each function
                       (partition 3) ;splits the rna string into a lazy sequence of characters eg (A U G) (A U G)
                       (map #(apply str %))  ;applies the fn "apply" onto the lazy sequence to create a new lazy sequence of strings ("AUG") ("AUG")
-                      (map #(translate_codon_to_protein %)) ;applies the fn translate_codon_to_protein which returns the proteins
+                      (map #(convert-codon-to-protein %)) ;applies the fn convert-codon-to-protein which returns the proteins
                       (take-while #(not= "STOP" %))         ;Although not a complete copy, i got this code from https://stackoverflow.com/questions/11866446/how-to-stop-iterating-a-sequence-when-a-condition-is-met and used the clojure cheatsheet to further understand how it works and apply it to this program
-                      )]
-    result))
+                     )]
+    amino-acids))
 
 ;task 3
 
@@ -47,7 +74,7 @@
 ;5.
 (defn -main []
   (let [trinary-number "112"
-        decimal-equivalent (trinary-to-decimal trinary-number)]
+        decimal-equivalent (convert-trinary-to-decimal trinary-number)]
     (println (str "The decimal equivalent of " trinary-number " is " decimal-equivalent)))
-  (println(translate_rna_to_amino_acids "AUGUUUUGG" ))
+  (println(convert-rna-sequence-to-amino-acids "AUGUUUUGG" ))
   )
